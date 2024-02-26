0f8acc312e09a1e7fc89631e003c075bf1fa6ad5
Shivam Prajapati <100841132+Shivam36998@users.noreply.github.com>
2024-02-26 07:53:35 +0530

    Update api.js

--- public/api.js
+++ public/api.js
@@ -1,4 +1,4 @@
-const hostLink = "http://localhost:3000"
+const hostLink = "https://new-crm-theta.vercel.app/";
 
 const registerNewUserLink = `${hostLink}/auth/newUser`;
 const signinLink = `${hostLink}/auth/signin`;
